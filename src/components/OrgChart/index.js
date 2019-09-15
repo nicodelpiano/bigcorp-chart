60bbd9822370afcc200606c1fd20b7d728fb95bd
Nicolas Del Piano <ndel314@gmail.com>
2019-09-15 16:58:23 -0300

    Indent OrgTree component

--- src/components/OrgChart/index.js
+++ src/components/OrgChart/index.js
@@ -1,44 +1,44 @@
 import React, { useState } from 'react'
 import Employee from '../Employee'
-import { isEmpty } from 'lodash'
+import { isEmpty, map } from 'lodash'
 
 function OrgChart({ tree }) {
   const [collapsed, setCollapsed] = useState(false)
-
   return (
-    !isEmpty(tree) &&
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        flexDirection: 'column',
-        border: '3px solid blue'
-      }}
-    >
-      <Employee
-        employee={tree.data}
-        onClick={() => setCollapsed(!collapsed)}
-        collapsed={collapsed}
-        displayButton={tree.children && tree.children.length > 0}
-      />
-      {tree.children && (
-        <div
-          style={{
-            display: !collapsed && tree.children.length > 0 ? 'flex' : 'none',
-            justifyContent: 'space-between',
-            padding: 10,
-            alignItems: 'flex-start',
-            flexDirection: 'row',
-            border: '3px solid red'
-          }}
-        >
-          {tree.children.map((child, index) => (
-            <OrgChart tree={child} key={index} />
-          ))}
-        </div>
-      )}
-    </div>
+    !isEmpty(tree) && (
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          flexDirection: 'column',
+          border: '3px solid blue'
+        }}
+      >
+        <Employee
+          employee={tree.data}
+          onClick={() => setCollapsed(!collapsed)}
+          collapsed={collapsed}
+          displayButton={!isEmpty(tree.children)}
+        />
+        {!isEmpty(tree.children) && (
+          <div
+            style={{
+              display: !collapsed ? 'flex' : 'none',
+              justifyContent: 'space-between',
+              padding: 10,
+              alignItems: 'flex-start',
+              flexDirection: 'row',
+              border: '3px solid red'
+            }}
+          >
+            {map(tree.children, (child, index) => (
+              <OrgChart tree={child} key={index} />
+            ))}
+          </div>
+        )}
+      </div>
+    )
   )
 }
 
