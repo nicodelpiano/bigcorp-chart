b5006f79f94e66bf90aafa9590259988880fac61
Nicolas Del Piano <ndel314@gmail.com>
2019-09-15 15:35:02 -0300

    Added tests for orgChart implementation

---
+++ src/tests/orgChart.spec.js
@@ -0,0 +1,209 @@
+import OrgChart from '../utils/orgChart'
+
+const data = [
+  {
+    first: 'Patricia',
+    last: 'Diaz',
+    id: 1,
+    manager: 9,
+    department: 5,
+    office: 2
+  },
+  {
+    first: 'Daniel',
+    last: 'Smith',
+    id: 2,
+    manager: 9,
+    department: 5,
+    office: 2
+  },
+  {
+    first: 'Thomas',
+    last: 'Parker',
+    id: 3,
+    manager: 9,
+    department: 4,
+    office: null
+  },
+  {
+    first: 'Stephen',
+    last: 'Baker',
+    id: 9,
+    manager: 0,
+    department: 2,
+    office: 1
+  }
+]
+
+describe('orgChart', () => {
+  describe('in an empty organization', () => {
+    const emptyOrgChart = new OrgChart()
+    test('size should be zero', () => {
+      expect(emptyOrgChart.size()).toEqual(0)
+    })
+    test('it should return undefined when searching for an employee', () => {
+      expect(emptyOrgChart.get(0)).toEqual(undefined)
+    })
+    test('it should draw an empty chart', () => {
+      expect(emptyOrgChart.draw()).toEqual({})
+    })
+  })
+
+  describe('.get', () => {
+    const orgChart = new OrgChart()
+    orgChart.insertMultiple(data)
+
+    test('it should return an existent employee', () => {
+      expect(orgChart.get(9).id).toEqual(9)
+    })
+    test('it should return undefined for a non-existent employee', () => {
+      expect(orgChart.get(99)).toEqual(undefined)
+    })
+  })
+
+  describe('.insert', () => {
+    const orgChart = new OrgChart()
+
+    test('it should insert new employees correctly', () => {
+      const employee = {
+        first: 'John',
+        last: 'Doe',
+        id: 100,
+        manager: 1,
+        department: 5,
+        office: 2
+      }
+      orgChart.insert(employee)
+      expect(orgChart.get(100)).toEqual({ ...employee, children: new Set() })
+    })
+
+    test('it should be idempotent', () => {
+      const employee = {
+        first: 'John',
+        last: 'Doe',
+        id: 101,
+        manager: 1,
+        department: 5,
+        office: 2
+      }
+      orgChart.insert(employee)
+      const orgChartSizeBefore = orgChart.size()
+      orgChart.insert(employee)
+      expect(orgChart.size()).toEqual(orgChartSizeBefore)
+    })
+
+    test('it should update an already present employee', () => {
+      const employee = {
+        first: 'John',
+        last: 'Doe',
+        id: 102,
+        manager: 1,
+        department: 5,
+        office: 2
+      }
+      const employee2 = {
+        first: 'John',
+        last: 'Doe',
+        id: 102,
+        manager: 1,
+        department: 5,
+        office: 3
+      }
+      orgChart.insert(employee).insert(employee2)
+      expect(orgChart.get(102).office).toEqual(3)
+    })
+
+    test(`it should update employee's manager correctly`, () => {
+      const manager = {
+        first: 'Jorge',
+        last: 'Manager',
+        id: 40,
+        manager: 0,
+        department: 5,
+        office: 2
+      }
+      orgChart.insert(manager)
+      const managerChildrenSize = orgChart.get(40).children.size
+      const employee = {
+        first: 'John',
+        last: 'Doe',
+        id: 103,
+        manager: 40,
+        department: 5,
+        office: 2
+      }
+      orgChart.insert(employee)
+      expect(orgChart.get(40).children.size).toEqual(managerChildrenSize + 1)
+    })
+  })
+
+  describe('.insertMultiple', () => {
+    test('should insert multiple employees correctly', () => {
+      const orgChart = new OrgChart()
+      orgChart.insertMultiple(data)
+      expect(orgChart.size()).toEqual(data.length)
+    })
+  })
+
+  describe('.draw', () => {
+    const orgChart = new OrgChart()
+    orgChart.insertMultiple(data)
+    const chart = orgChart.draw()
+    console.log(chart)
+
+    test('it should draw the chart correctly', () => {
+      expect(chart).toEqual({
+        id: 9,
+        data: {
+          id: 9,
+          office: 1,
+          department: 2,
+          first: 'Stephen',
+          last: 'Baker'
+        },
+        children: [
+          {
+            id: 1,
+            data: {
+              department: 5,
+              first: 'Patricia',
+              id: 1,
+              last: 'Diaz',
+              office: 2
+            },
+            children: []
+          },
+          {
+            id: 2,
+            data: {
+              department: 5,
+              first: 'Daniel',
+              id: 2,
+              last: 'Smith',
+              office: 2
+            },
+            children: []
+          },
+          {
+            id: 3,
+            data: {
+              department: 4,
+              first: 'Thomas',
+              id: 3,
+              last: 'Parker',
+              office: null
+            },
+            children: []
+          }
+        ]
+      })
+    })
+
+    test('it should have the manager as root of the chart', () => {
+      const { department, first, last, id, office, children } = orgChart.get(9)
+      expect(chart.id).toEqual(9)
+      expect(chart.data).toEqual({ department, first, last, id, office })
+      expect(chart.children.length).toEqual(children.size)
+    })
+  })
+})
--- src/utils/orgchart.js
+++ src/utils/orgchart.js
@@ -1,21 +1,24 @@
 class OrgChart {
   constructor(rootManagerValue = 0) {
     // Value to look up when inserting employees to set which one
-    // is the root manager
+    // is the root manager.
     this._rootManagerValue = rootManagerValue
-    // Root manager id. Will be filled when found in the data.
+    // Root manager id, to keep track of the manager when found.
     this._rootManagerId = null
+    // The chart for the organization will be a map,
+    // where keys are employee ids, and values are their data
+    // plus a set with children ids, if any.
     this._orgChart = new Map()
-  }
-
-  _getEmployee(employeeId) {
-    return this._orgChart.get(employeeId)
   }
 
   // Exports the underlying data structure of the tree to a traversable
   // representation intended for displaying the whole organizational structure
   _getHierarchy(employeeId = this._rootManagerId) {
-    const { id, first, last, office, department, children } = this._getEmployee(
+    if (!employeeId) {
+      return {}
+    }
+
+    const { id, first, last, office, department, children } = this.get(
       employeeId
     )
 
@@ -36,12 +39,11 @@ class OrgChart {
     }
   }
 
-  // Upsert
   insert(employee) {
     const { id, manager } = employee
-    const employeeEntry = this._getEmployee(id)
+    const employeeEntry = this.get(id)
 
-    // Upsert employee entry
+    // Upsert an employee: data will be overwritten if it already exists.
     this._orgChart.set(id, {
       children: new Set(),
       ...employeeEntry,
@@ -49,18 +51,20 @@ class OrgChart {
     })
 
     if (manager !== this._rootManagerValue) {
-      const managerEntry = this._getEmployee(manager)
-      // Update manager's entry if it differs from the root key
+      // Update manager's entry if it differs from the root key.
+      const managerEntry = this.get(manager)
+      // Recall that we create the managers entry here always: it will be
+      // updated when we actually insert the manager's node.
       this._orgChart.set(manager, {
         ...managerEntry,
         children: managerEntry ? managerEntry.children.add(id) : new Set([id])
       })
     } else {
-      // This is the root manager
+      // This is the root manager, and thus we update the variable.
       this._rootManagerId = id
     }
 
-    // To chain multiple calls
+    // To chain multiple calls.
     return this
   }
 
@@ -69,7 +73,15 @@ class OrgChart {
     return this
   }
 
-  drawChart() {
+  get(employeeId) {
+    return this._orgChart.get(employeeId)
+  }
+
+  size() {
+    return this._orgChart.size
+  }
+
+  draw() {
     console.log('Drawing chart...')
     return this._getHierarchy()
   }
